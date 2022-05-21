d8d138d3ca7cb53d83aa351ac44321b514936a19
jaden221 <55807354+jaden221@users.noreply.github.com>
2022-05-21 11:49:35 -0400

    Added Ext Assets Folder

--- GameJamMay20/Assets/Scripts/Combat/Delfect.cs
+++ GameJamMay20/Assets/Scripts/Combat/Delfect.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Delfect : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void HandleDamageReceived(DamageDataReceived damageData)
+    {
+        // only affect stats if deflecting
     }
 }
