3d1f017169f828e6d62eef40cd5472c9bc0bc0b5
Alistair Fyfe <38055484+fyfey96@users.noreply.github.com>
2022-05-22 16:47:08 +0100

    Updated Menu Scripts to Make more Modular

---
+++ GameJamMay20/Assets/Scripts/GUI/ToggleAudio.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleAudio : MonoBehaviour
+{  
+    bool _musicIsOn = true;
+    AudioSource _audioSource;
+
+    void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Toggle()
+    {
+        if (_musicIsOn)
+        {
+            _audioSource.Stop();
+            _musicIsOn = false;
+        }
+        else if (!_musicIsOn)
+        {
+            _audioSource.Play();
+            _musicIsOn = true;
+        }
+    }
+}
---
+++ GameJamMay20/Assets/Scripts/GUI/ToggleSprite.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleSprite : MonoBehaviour
+{
+    [SerializeField] Sprite _spriteA;
+    [SerializeField] Sprite _spriteB;
+    
+    Sprite _currentSprite;
+   
+    bool _toggle = false;
+
+    public void Toggle()
+    {
+        if (_toggle)
+        {
+            GetComponent<Image>().sprite = _spriteA;
+            _toggle = false;
+        }
+        else if (!_toggle)
+        {
+            GetComponent<Image>().sprite = _spriteB;
+            _toggle = true;
+        }
+    }
+
+
+    
+}
