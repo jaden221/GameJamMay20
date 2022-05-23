25702dd74b59be965346a41120ebcac0579a3ce3
jaden221 <55807354+jaden221@users.noreply.github.com>
2022-05-23 09:43:21 -0400

    + Block&Deflect functionality, Flipsprite now input based

--- GameJamMay20/Assets/Scripts/Movement/FlipSprite.cs
+++ GameJamMay20/Assets/Scripts/Movement/FlipSprite.cs
@@ -1,20 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FlipSprite : StateMachineBehaviour
 {
-    Rigidbody2D rigidbody;
+    PlayerInputMap input;
+    InputAction move;
     SpriteRenderer sprite;
 
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         if (sprite == null) sprite = animator.GetComponent<SpriteRenderer>();
-        if (rigidbody == null) rigidbody = animator.transform.root.GetComponentInChildren<Rigidbody2D>();
+
+        if (move.ReadValue<Vector2>().x < 0)
+        {
+            sprite.flipX = true;
+        }
+        else
+        {
+            sprite.flipX = false;
+        }
     }
 
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        sprite.flipX = rigidbody.velocity.x < 0;
+        if (move.ReadValue<Vector2>().x < 0)
+        {
+            sprite.flipX = true;
+        }
+        else
+        {
+            sprite.flipX = false;
+        }
+    }
+
+    void OnEnable()
+    {
+        if (input == null) input = new PlayerInputMap();
+        move = input.Player.Move;
+        move.Enable();
+    }
+
+    void OnDisable()
+    {
+        move.Disable();
     }
 }
---
+++ GameJamMay20/Assets/Scripts/Player/Ability1.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Ability1 : MonoBehaviour
+{
+	
+
+	
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
