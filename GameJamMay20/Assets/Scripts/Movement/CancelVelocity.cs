297434dcadf3a3eab1f11fc541a6c4c95170ab34
jaden221 <55807354+jaden221@users.noreply.github.com>
2022-05-23 18:14:08 -0400

    Cancel Velocity works in update now

--- GameJamMay20/Assets/Scripts/Movement/CancelVelocity.cs
+++ GameJamMay20/Assets/Scripts/Movement/CancelVelocity.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CancelVelocity : StateMachineBehaviour
 {
+    [SerializeField] bool cancelUpdate = true;
+
     Rigidbody2D rigidbody;
 
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -11,5 +13,13 @@ public class CancelVelocity : StateMachineBehaviour
         if (rigidbody == null) rigidbody = animator.GetComponent<Rigidbody2D>();
 
         rigidbody.velocity = Vector2.zero;
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (cancelUpdate)
+        {
+            rigidbody.velocity = Vector2.zero;
+        }
     }
 }
