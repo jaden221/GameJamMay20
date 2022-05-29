77b25b70a7a3c3452f7de41a320f71f7bd76fd5a
jaden221 <55807354+jaden221@users.noreply.github.com>
2022-05-29 18:44:28 -0400

    + World Space Health

--- GameJamMay20/Assets/Scripts/GUI/HealthBar.cs
+++ GameJamMay20/Assets/Scripts/GUI/HealthBar.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-//Written by Fyfey96
-
 public class HealthBar : MonoBehaviour
 {
     Image _fillImage;
-
-    [SerializeField] Health _health;
+    [SerializeField] bool autoFillHealth = true;
+    [SerializeField] Health health;
 
     void Awake()
     {
         _fillImage = GetComponent<Image>();
+        if (autoFillHealth) health = transform.root.GetComponent<Health>();
+        if (health == null) Debug.Log($"Health is null on {transform.name}"); ;
     }
 
     void Update()
     {
-        _fillImage.fillAmount = _health.GetHealthPercent;
+        _fillImage.fillAmount = health.GetHealthPercent;
     }
 }
