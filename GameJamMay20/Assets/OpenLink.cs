835183f6b8674453f96e035ca3409cf57552b026
nuhvy <74287243+nuhvy@users.noreply.github.com>
2022-05-30 14:54:27 -0500

    ...

---
+++ GameJamMay20/Assets/OpenLink.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void OpenChannel()
+    {
+        Application.OpenURL("https://github.com/jaden221/GameJamMay20");
+    }
+}
