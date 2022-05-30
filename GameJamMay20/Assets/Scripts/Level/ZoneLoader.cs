af77a5d449772324e9894ad019d0b07fd079db7b
nuhvy <74287243+nuhvy@users.noreply.github.com>
2022-05-29 19:50:26 -0500

    Made stuff do the works

--- GameJamMay20/Assets/Scripts/Level/ZoneLoader.cs
+++ GameJamMay20/Assets/Scripts/Level/ZoneLoader.cs
@@ -5,25 +5,25 @@ using UnityEngine;
 
 public class ZoneLoader : MonoBehaviour
 {
-    //For setting where the camera is going to go when the player enters the area
-    //You can set this to the coordinates of where the new ground is at as the ground prefab is the exact size of the camera.
+    [Tooltip("For setting where the camera is going to go when the player enters the area. You can set this to the coordinates of where the new ground is at as the ground prefab is the exact size of the camera.")]
     [SerializeField] Vector3 newCameraPos;
-    //Set all the enemy spawners you want in the zone in this array.
-    //make sure the spawners are actually in this zone so you dont spawn enemies in other zones.
+    [Tooltip("Set all the enemy spawners you want in the zone in this array. Make sure the spawners are actually in this zone so you dont spawn enemies in other zones.")]
     [SerializeField] EnemySpawner[] enemySpawners;
-    //Set the gates that are going to open up in this array in editor.
-    //Since there isnt a gate opening animation right now these gates will just get disabled.
+    [Tooltip("Place the gate objects in here")]
     [SerializeField] GameObject[] gates;
-    //Set an empty at the location you want the player to start in the new zone in here.
+    [Tooltip("Place Images of what the open gates will look like in the same order the gates are in")]
+    [SerializeField] Sprite[] openGates;
+    [Tooltip("Set an empty at the location you want the player to start in the new zone in here.")]
     [SerializeField] Transform playerStartPoint;
-
-    [SerializeField] float cameraTransitionSpeed = .5f;
-
-    [SerializeField] float playerTransitionSpeed = .5f;
+    [SerializeField] float cameraTransitionSpeed = .01f;
+    [SerializeField] float playerTransitionSpeed = .01f;
 
     GameObject player;
     LevelHandeler levelHandeler;
-    [SerializeField] bool isZoneReady = true;
+
+    bool isZoneReady = true;
+    Vector3 cameraOGPos;
+    Vector2 playerOGPos;
     
 
     public void Start()
@@ -73,16 +73,27 @@ public class ZoneLoader : MonoBehaviour
         for (int i = 0; i < gates.Length; i++)
         {
             GameObject gate = gates[i];
-            gate.SetActive(false);
+            if (openGates != null)
+            {
+                Sprite openGate = openGates[i];
+                gate.GetComponent<SpriteRenderer>().sprite = openGate;
+                gate.GetComponent<BoxCollider2D>().enabled = false;
+            }
+            else
+            {
+                gate.SetActive(false);
+            }
         }
     }
 
     private void Transition()
     {
         player.GetComponent<PlayerController>().DisableMove();
-        Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, newCameraPos,  cameraTransitionSpeed);
-        player.transform.position = Vector2.Lerp(player.transform.position, playerStartPoint.position, playerTransitionSpeed);
-        if(Vector3.Distance(Camera.main.transform.position, newCameraPos) <= 0.1f && Vector2.Distance(player.transform.position, playerStartPoint.position) <= 0.1f)
+        cameraOGPos = Camera.main.transform.position;
+        playerOGPos = player.transform.position;
+        Camera.main.transform.position = Vector3.Lerp(cameraOGPos, newCameraPos,  cameraTransitionSpeed);
+        player.transform.position = Vector3.Lerp(playerOGPos, playerStartPoint.position, playerTransitionSpeed);
+        if(Vector3.Distance(Camera.main.transform.position, newCameraPos) == 0f && Vector2.Distance(player.transform.position, playerStartPoint.position) == 0f)
         {
             Camera.main.transform.position = newCameraPos;
             player.transform.position = playerStartPoint.position;
